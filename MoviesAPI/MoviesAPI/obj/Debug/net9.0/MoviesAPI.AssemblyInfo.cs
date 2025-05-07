7da6224ddfaab901c2e49a20fd4f5f967215f04c
bryan-ignacio <bryan-ignacio@outlook.com>
2025-05-07 17:28:58 -0600

    updated.

--- MoviesAPI/MoviesAPI/obj/Debug/net9.0/MoviesAPI.AssemblyInfo.cs
+++ MoviesAPI/MoviesAPI/obj/Debug/net9.0/MoviesAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+487cca9f886a7e6e7d12ad2e5c081747d25be5dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13b1bc32fec44653bfb19a7828ba9c7304a011fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
