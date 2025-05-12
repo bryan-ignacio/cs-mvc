88e2fecdc75c68f305839e07f696283bae39ac64
bryan-ignacio <bryan-ignacio@outlook.com>
2025-05-11 20:43:24 -0600

    updated.

--- MoviesAPI/MoviesAPI/Controllers/CategoriasController.cs
+++ MoviesAPI/MoviesAPI/Controllers/CategoriasController.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using MoviesAPI.Models.Dtos;
 using MoviesAPI.Repository.IRepository;
 
 namespace MoviesAPI.Controllers
@@ -12,9 +14,26 @@ namespace MoviesAPI.Controllers
         private readonly ICategoriaRepository _ctRepo;
         private readonly IMapper _mapper;
 
-        public CategoriasController(ICategoriaRepository ctRepo)
+        public CategoriasController(ICategoriaRepository ctRepo, IMapper mapper)
         {
             this._ctRepo = ctRepo;
+            this._mapper = mapper;
         }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetCategorias()
+        {
+            var ListaCategorias = this._ctRepo.GetCategorias();
+            var listaCategoriasDto = new List<CategoriaDto>();
+            foreach (var lista in ListaCategorias)
+            {
+                listaCategoriasDto.Add(this._mapper.Map<CategoriaDto>(lista));
+            }
+            return Ok(listaCategoriasDto);
+        }
+
+        
     }
 }
